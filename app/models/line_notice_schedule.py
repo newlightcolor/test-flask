908da81d0fda6b403b9f062252ecfdbec35f0cea
newlightcolor <newlightcolor@gmail.com>
2022-06-21 00:00:05 +0900

    モデルはこの関数があった方が好み

--- app/models/line_notice_schedule.py
+++ app/models/line_notice_schedule.py
@@ -10,3 +10,56 @@ class LineNoticeSchedule(db.Model):
     schedule = db.Column(db.DateTime)
     message = db.Column(db.String(100), default='通知だよ～')
     finished = db.Column(db.Integer, default=0)
+
+    #SELECT
+    def get_list(where=dict(), limit=0):
+        line_notice_schedule = db.session.query(LineNoticeSchedule)
+
+        #WHERE
+        if 'id' in where:
+            line_notice_schedule = line_notice_schedule.filter(LineNoticeSchedule.id == where['id'])
+        if 'line_user_id' in where:
+            line_notice_schedule = line_notice_schedule.filter(LineNoticeSchedule.line_user_id == where['line_user_id'])
+        if 'schedule' in where:
+            line_notice_schedule = line_notice_schedule.filter(LineNoticeSchedule.schedule == where['schedule'])
+        if 'finished' in where:
+            line_notice_schedule = line_notice_schedule.filter(LineNoticeSchedule.finished == where['finished'])
+
+        # 複数レコードなら辞書で返す
+        if limit == 1:
+            return line_notice_schedule.first()
+        else :
+            return line_notice_schedule.all()
+
+    #INSERT
+    def insert(line_user_id:str, schedule:str = None, message:str = None):
+        line_notice_schedule = LineNoticeSchedule(line_user_id=line_user_id, schedule=schedule, message=message)
+        db.session.add(line_notice_schedule)
+        db.session.commit()
+        return line_notice_schedule.id #insert_id
+
+    #UPDATE
+    def update(where=dict(), set=dict()):
+        line_notice_schedule = db.session.query(LineNoticeSchedule)
+
+        #WHERE
+        if 'id' in where:
+            line_notice_schedule = line_notice_schedule.filter(LineNoticeSchedule.id == where['id'])
+        if 'line_user_id' in where:
+            line_notice_schedule = line_notice_schedule.filter(LineNoticeSchedule.line_user_id == where['line_user_id'])
+        if 'schedule' in where:
+            line_notice_schedule = line_notice_schedule.filter(LineNoticeSchedule.schedule == where['schedule'])
+        if 'finished' in where:
+            line_notice_schedule = line_notice_schedule.filter(LineNoticeSchedule.finished == where['finished'])
+
+        # SET
+        line_notice_schedules = line_notice_schedule.all()
+        for record in line_notice_schedules:
+            if 'schedule' in set:
+                record.schedule=set['schedule']
+            if 'message' in set:
+                record.message=set['message']
+            if 'finished' in set:
+                record.finished=set['finished']
+
+        db.session.commit()
--- app/models/line_user.py
+++ app/models/line_user.py
@@ -7,25 +7,64 @@ class LineUser(db.Model):
     __tablename__ = 'line_users'
 
     id = db.Column(db.Integer, primary_key=True)
-    line_user_id = db.Column(db.String(100))
-    name = db.Column(db.String(255))
-    comment = db.Column(db.Text)
-    pic_url = db.Column(db.Text)
+    line_user_id = db.Column(db.String(100), nullable=True)
+    name = db.Column(db.String(255), nullable=True)
+    comment = db.Column(db.Text, nullable=True)
+    pic_url = db.Column(db.Text, nullable=True)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
+    # SELET
     def get_list(where=dict(), limit=0):
-        line_users = LineUser
+        line_users = db.session.query(LineUser)
 
+        # WHERE
         if 'id' in where:
-            line_users = line_users.query.filter(line_users.id == where['id'])
+            line_users = line_users.filter(LineUser.id == where['id'])
         if 'line_user_id' in where:
-            line_users = line_users.query.filter(line_users.line_user_id == where['line_user_id'])
+            line_users = line_users.filter(LineUser.line_user_id == where['line_user_id'])
         if 'name' in where:
-            line_users = line_users.query.filter(line_users.name == where['name'])
+            line_users = line_users.filter(LineUser.name == where['name'])
 
-        # 複数レコードなら辞書で返す
+        # 複数レコードならリスト
         if limit == 1:
             return line_users.first()
         else :
             return line_users.all()
+
+    # INSERT
+    def insert(line_user_id:str, name:str = None, pic_url:str = None):
+        line_user = LineUser(line_user_id=line_user_id, name=name, pic_url=pic_url)
+        db.session.add(line_user)
+        db.session.commit()
+        return line_user.id #insert_id
+
+    # UPDATE
+    def update(where=dict(), set=dict()):
+        line_user = db.session.query(LineUser)
+
+        # WHERE
+        if 'id' in where:
+            line_user = line_user.filter(LineUser.id == where['id'])
+        if 'line_user_id' in where:
+            line_user = line_user.filter(LineUser.line_user_id == where['line_user_id'])
+        if 'latitude' in where:
+            line_user = line_user.filter(LineUser.latitude == where['latitude'])
+        if 'longitude' in where:
+            line_user = line_user.filter(LineUser.longitude == where['longitude'])
+
+        # SET
+        line_users = line_user.all()
+        for record in line_users:
+            if 'name' in set:
+                record.schedule=set['name']
+            if 'pic_url' in set:
+                record.message=set['pic_url']
+            if 'latitude' in set:
+                record.latitude=set['latitude']
+            if 'longitude' in set:
+                record.longitude=set['longitude']
+
+        db.session.commit()
